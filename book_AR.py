5b1fabee0fa75163aa145e462354b410f6e83781
shubham1594 <72883560+shubham1594@users.noreply.github.com>
2021-03-24 13:03:03 +0530

    Add files via upload

---
+++ book_AR.py
@@ -0,0 +1,51 @@
+# implementing Apriori algorithm from mlxtend
+import pandas as pd
+from mlxtend.frequent_patterns import apriori,association_rules
+import matplotlib.pyplot as plt
+
+#importing dattaset
+book = pd.read_csv("book.csv")
+
+book_list = [i for item in book for i in item]
+from collections import Counter
+item_frequencies = Counter(book_list)
+item_frequencies = sorted(item_frequencies.items(),key = lambda x:x[1])
+
+# Storing frequencies and items in separate variables 
+frequencies = list(reversed([i[1] for i in item_frequencies]))
+items = list(reversed([i[0] for i in item_frequencies]))
+
+# barplot of top 10 
+plt.bar(height = frequencies[0:11],x = list(range(0,11)),color='rgbkymc');plt.xticks(list(range(0,11),),items[0:11]);plt.xlabel("items")
+plt.ylabel("Count")
+
+
+# Most Frequent item sets based on support 
+frequent_itemsets = apriori(book, min_support=0.005, max_len=3,use_colnames = True)
+frequent_itemsets.sort_values('support',ascending = False,inplace=True)
+plt.bar(x = list(range(1,11)),height = frequent_itemsets.support[1:11],color='rgmyk');plt.xticks(list(range(1,11)),frequent_itemsets.itemsets[1:11])
+plt.xlabel('item-sets');plt.ylabel('support')
+
+#sorting using rules
+rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
+rules.sort_values('lift',ascending = False,inplace=True)
+
+ 
+# Eliminating Redudancy in Rules
+def to_list(i):
+    return (sorted(list(i)))
+
+
+ma_X = rules.antecedents.apply(to_list)+rules.consequents.apply(to_list)
+ma_X = ma_X.apply(sorted)
+rules_sets = list(ma_X)
+
+unique_rules_sets = [list(m) for m in set(tuple(i) for i in rules_sets)]
+
+index_rules = []
+for i in unique_rules_sets:
+    index_rules.append(rules_sets.index(i))
+
+
+# getting rules without any redudancy 
+rules_no_redudancy  = rules.iloc[index_rules,:]
